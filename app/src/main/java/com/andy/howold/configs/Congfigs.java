13b0bd91097bf04ff8413b408e1c20837f5399e2
andy521zhufuli <352605880@qq.com>
2015-07-22 16:07:10 +0800

    从eclipse到Android studio
    转移到studio  因为google已经不支持eclipse了

---
+++ app/src/main/java/com/andy/howold/configs/Congfigs.java
@@ -0,0 +1,8 @@
+package com.andy.howold.configs;
+
+public class Congfigs
+{
+	// sharesdk
+	public static final String ShareSDK_AppKey = "796fa8514fca";
+	public static final String ShareSDK_AppSecret = "27464131aab87bb18f9f7717b09e2136";
+}
